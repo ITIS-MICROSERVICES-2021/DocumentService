66c041380f3083eca190fc5d271b183f464d49b1
ARManakhov <ARManakhov@yandex.ru>
2021-09-29 17:16:40 +0300

    project init

---
+++ MicroservicesCmd3/Controllers/SuffixController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MicroservicesCmd3.Controllers
+{
+    [ApiController]
+    [Route("/api/suffix")]
+    public class SuffixController : ControllerBase
+    {
+        private readonly ILogger<SuffixController> _logger;
+
+        public SuffixController(ILogger<SuffixController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get(string text)
+        {
+            return text + "_" + Guid.NewGuid();
+        }
+    }
+}
