0d06d07e5463e5bf84041d3cb2e85c88626edb72
Ravil <graf.rav@gmail.com>
2021-10-23 21:57:25 +0300

    я хотел отрефакторить, но передумал, сами пусть говно выкавыривают

--- DocumentService/Startup.cs
+++ DocumentService/Startup.cs
@@ -1,3 +1,4 @@
+using DocumentService.Options;
 using DocumentService.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -23,6 +24,7 @@ namespace DocumentService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions<RedisOptions>("Redis");
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
